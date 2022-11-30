ef2bf3bd14a57602404511de69a9dd74a0fd0470
Yurii0909 <118471966+Yurii0909@users.noreply.github.com>
2022-12-01 00:02:29 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,12 @@
-﻿Console.WriteLine("Задача 10");
- //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-//456 -> 5
-//782 -> 8
-//918 -> 1
-int a = 456;
-int b = 782;
-int c = 918;
-int a1 = a / 10;
-int a2 = a1 % 10;
-Console.WriteLine ($"{a2}");
-int b1 = b / 10;
-int b2 = b1 % 10;
-Console.WriteLine ($"{b2}");
-int c1 = c / 10;
-int c2 = c1 % 10;
-Console.WriteLine ($"{c2}");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-int number = ReadInt("Введите трехзначное число: ");
-int amount = number.ToString().Length;
 
-if (amount < 3 || amount > 3)
-{
-    Console.WriteLine("Вы ввели не трехзначное число");
-}
-else
-{
-    Console.WriteLine(InCenter(number)); 
-}
-
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
 
 int ReadInt(string message)
 {
@@ -35,9 +14,23 @@ int ReadInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int InCenter(int a4)
+
+int MakeArray(int a, int b)
 {
-    
-    int result = ((a4 / 10) % 10);
-    return result;
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
 }
